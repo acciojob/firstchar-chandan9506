3d9d0723abd80fc6ef2533af71fffc6408840399
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-13 16:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstChar(text) {
+function firstChar(s) {
   // your code here
+	
+	if (s=="" || s== " " ) {
+		return "";
+	}
+	else{
+		let s1 =s.trim();
+		return s1[0];
+	} 
 }
 
 // Do not change the code below
-
 const text = prompt("Enter text:");
 alert(firstChar(text));
